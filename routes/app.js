20253199cf9d2683cc3940c6d84919fdd26c2b63
savvaniss <savvaniss@yahoo.gr>
2017-07-07 13:42:17 +0300

    add a route outsite angular to check node routing

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,15 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+// add a custom message route from below post request
+router.get('/message/:msg', function (req, res, next) {
+    res.render('message', {message: req.params.msg});
+});
+
+//request from form
+router.post('/message', function(req, res, next) {
+    var message = req.body.message;
+    res.redirect('/message/' + message);
+});
+
 module.exports = router;
